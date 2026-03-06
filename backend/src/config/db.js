ce1613315a8f135f440b8b4642174acf1c362735
felixparejapmdit <155054492+felixparejapmdit@users.noreply.github.com>
2026-03-06 15:36:27 +0800

    update

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,11 +1,14 @@
-
 const { Sequelize } = require('sequelize');
 const path = require('path');
 
 const sequelize = new Sequelize({
     dialect: 'sqlite',
     storage: path.join(__dirname, '../../../directus/database/data.db'),
-    logging: false
+    logging: false,
+    dialectOptions: {
+        // Simple mode for cross-platform compatibility
+        mode: 2,
+    }
 });
 
 module.exports = sequelize;
