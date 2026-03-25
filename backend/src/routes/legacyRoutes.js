ab92d4977e7f68647ec228b84a0fa54c718dab01
felixparejapmdit <155054492+felixparejapmdit@users.noreply.github.com>
2026-03-25 11:58:40 +0800

    docker

--- backend/src/routes/legacyRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const LegacyController = require('../controllers/LegacyController');
-
-// Replica of the letters_detailed.php API
-router.get('/letters', LegacyController.getDetailedLetters);
-
-module.exports = router;
